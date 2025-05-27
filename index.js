7b2438016cca28575bfb1f29414fc906f52c8b12
killergames3 <killerhackcodeup@gmail.com>
2025-05-26 23:05:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5501',
   'http://localhost:5501',
   'https://grasslandforest.xyz',
-  'effortless-profiterole-45a4cb.netlify.app'
+  'https://effortless-profiterole-45a4cb.netlify.app'
 ];
 
 app.use(cors({
